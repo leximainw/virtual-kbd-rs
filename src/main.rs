f0692f523a7fa62464c06ef5da1589c20653d263
leximainw <leximainw@gmail.com>
2022-12-02 10:27:37 -0500

    feat: implement naive non-wrapping solution

--- src/main.rs
+++ src/main.rs
@@ -51,9 +51,67 @@ fn kbd_input(commands: &str) -> Result<String, Box<dyn Error>> {
     Ok(text)
 }
 
+fn sol_naive_nonwrapping(input: &str) -> Result<String, Box<dyn Error>> {
+    let mut curr_x = 0;
+    let mut curr_y = 0;
+    let mut caps = false;
+    let mut commands = String::new();
+
+    for c in input.chars() {
+        if c == ' ' {
+            commands.push('Y');
+            continue;
+        }
+
+        let (curr_chars, flip_chars) = if caps {
+            (UPPER_CHARS, LOWER_CHARS)
+        } else {
+            (LOWER_CHARS, UPPER_CHARS)
+        };
+        let (new_x, new_y, flip) = if let Some(index) = curr_chars.iter().position(|x| *x == c) {
+            (index % 10, index / 10, false)
+        } else if let Some(index) = flip_chars.iter().position(|x| *x == c) {
+            (index % 10, index / 10, true)
+        } else {
+            return Err(Box::from("encountered out-of-range character {c}"));
+        };
+
+        if curr_y != new_y {
+            if curr_y < new_y {
+                commands.push_str(&"D".repeat(new_y - curr_y));
+            } else {
+                commands.push_str(&"U".repeat(curr_y - new_y));
+            }
+        }
+
+        if curr_x != new_x {
+            if curr_x < new_x {
+                commands.push_str(&"R".repeat(new_x - curr_x));
+            } else {
+                commands.push_str(&"L".repeat(curr_x - new_x));
+            }
+            curr_x = new_x;
+        }
+
+        if flip {
+            caps = !caps;
+            commands.push('C');
+        }
+
+        commands.push('A');
+    }
+
+    Ok(commands)
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::kbd_input;
+    use std::error::Error;
+
+    use super::{
+        kbd_input,
+        sol_naive_nonwrapping,
+    };
 
     #[test]
     fn test_kbd_input() {
@@ -61,5 +119,19 @@ mod tests {
         assert_eq!(kbd_input("A").unwrap(), "1");
         assert_eq!(kbd_input("DCACA").unwrap(), "Qq");
         assert_eq!(kbd_input("ADDDDAUUUUARRRRRRRRRRALLLLLLLLLLA").unwrap(), "11111");
+    }
+
+    #[test]
+    fn test_naive_nonwrapping() {
+        test_correctness(Box::new(sol_naive_nonwrapping));
+    }
+
+    fn test_correctness(solution: Box<dyn Fn(&str) -> Result<String, Box<dyn Error>>>) {
+        let test_vectors = [
+            "101",
+        ];
+        for test_vector in test_vectors {
+            assert_eq!(kbd_input(&solution(test_vector).unwrap()).unwrap(), test_vector);
+        }
     }
 }
